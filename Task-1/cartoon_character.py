d26bc74d56800018e9d99c9eeb8cb99e60d2550d
Rafael Louie Miguel <118577897+egg-lou@users.noreply.github.com>
2024-03-25 14:53:20 +0800

    init

---
+++ Task-1/cartoon_character.py
@@ -0,0 +1,13 @@
+def display_pingu():
+    pingu = [
+        "    _   ", 
+        "  ('v') ", 
+        "  // \\\\ ", 
+        " (v_ _v)"
+        ]
+
+    for line in pingu:
+        print(line)
+
+if __name__ == "__main__":
+    display_pingu()
---
+++ Task-1/pup_hymn.py
@@ -0,0 +1,21 @@
+hymn = [
+    "Sintang Paaralan",
+    "Tanglaw ka ng bayan",
+    "Pandayan ng isip ng kabataan",
+    "Kami ay dumating nang salat sa yaman",
+    "Hanap na dunong ay iyong alay",
+    "Ang layunin mong makatao",
+    "Dinarangal ang Pilipino",
+    "Ang iyong aral, diwa, adhikang taglay",
+    "PUP, aming gabay",
+    "Paaralang dakila",
+    "PUP, pinagpala",
+    "Gagamitin ang karunungan",
+    "Mula sa iyo, para sa bayan",
+    "Ang iyong aral, diwa, adhikang taglay",
+    "PUP, aming gabay",
+    "Paaralang dakila",
+    "PUP, pinagpala"
+]
+
+
