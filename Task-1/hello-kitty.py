3efbb5c6b11d6708cd2ce5ab1888ff61606a5e5f
egglou <miguel.rafaellouie2003@gmail.com>
2024-03-28 20:15:31 +0800

    ayoko na

---
+++ Task-1/hello-kitty.py
@@ -0,0 +1,12 @@
+def display_hello_kitty():
+    hello_kitty = [
+        "  /\\_/\\  ",
+        " ( o.o ) ",
+        "  > ^ <  "
+        ]
+
+    for line in hello_kitty:
+        print(line)
+
+if __name__ == "__main__":
+    display_hello_kitty()
