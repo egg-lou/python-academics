1f6fa9d1dfafdfbc6061b844982897157afa96dc
egglou <miguel.rafaellouie2003@gmail.com>
2024-03-28 20:17:41 +0800

    done pup-hymn

--- Task-1/pup-hymn.py
+++ Task-1/pup-hymn.py
@@ -1,23 +1,26 @@
-hymn = [
-    "                 Sintang Paaralan",
-    "                Tanglaw ka ng bayan",
-    "            Pandayan ng isip ng kabataan",
-    "        Kami ay dumating nang salat sa yaman",
-    "            Hanap na dunong ay iyong alay",
-    "              Ang layunin mong makatao",
-    "              Dinarangal ang Pilipino",
-    "       Ang iyong aral, diwa, adhikang taglay",
-    "                  PUP, aming gabay",
-    "                  Paaralang dakila",
-    "                  PUP, pinagpala",
-    "             Gagamitin ang karunungan",
-    "            Mula sa iyo, para sa bayan",
-    "        Ang iyong aral, diwa, adhikang taglay",
-    "                  PUP, aming gabay",
-    "                  Paaralang dakila",
-    "                   PUP, pinagpala"
-]
+def print_tree_hymn():
+    hymn = [
+        "                  Sintang Paaralan",
+        "                Tanglaw ka ng bayan",
+        "            Pandayan ng isip ng kabataan",
+        "        Kami ay dumating nang salat sa yaman",
+        "            Hanap na dunong ay iyong alay",
+        "              Ang layunin mong makatao",
+        "              Dinarangal ang Pilipino",
+        "       Ang iyong aral, diwa, adhikang taglay",
+        "                  PUP, aming gabay",
+        "                  Paaralang dakila",
+        "                  PUP, pinagpala",
+        "             Gagamitin ang karunungan",
+        "            Mula sa iyo, para sa bayan",
+        "        Ang iyong aral, diwa, adhikang taglay",
+        "                  PUP, aming gabay",
+        "                  Paaralang dakila",
+        "                   PUP, pinagpala"
+    ]
 
-for line in hymn:
-    print(line)
+    for line in hymn:
+        print(line)
 
+if __name__ == "__main__":
+    print_tree_hymn()
