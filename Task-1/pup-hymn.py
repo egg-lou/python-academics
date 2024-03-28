3d59c4ff5722091fde02b0a4354709774500e264
egglou <miguel.rafaellouie2003@gmail.com>
2024-03-28 22:11:57 +0800

    done puphymn

--- Task-1/pup-hymn.py
+++ Task-1/pup-hymn.py
@@ -1,26 +1,33 @@
-def print_tree_hymn():
-    hymn = [
-        "                  Sintang Paaralan",
-        "                Tanglaw ka ng bayan",
-        "            Pandayan ng isip ng kabataan",
-        "        Kami ay dumating nang salat sa yaman",
-        "            Hanap na dunong ay iyong alay",
-        "              Ang layunin mong makatao",
-        "              Dinarangal ang Pilipino",
-        "       Ang iyong aral, diwa, adhikang taglay",
-        "                  PUP, aming gabay",
-        "                  Paaralang dakila",
-        "                  PUP, pinagpala",
-        "             Gagamitin ang karunungan",
-        "            Mula sa iyo, para sa bayan",
-        "        Ang iyong aral, diwa, adhikang taglay",
-        "                  PUP, aming gabay",
-        "                  Paaralang dakila",
-        "                   PUP, pinagpala"
-    ]
+def print_hymn():
+    hymn = (
+        "\nPUP Hymn\n"
+        "Sintang Paaralan\n"
+        "Tanglaw ka ng bayan\n"
+        "Pandayan ng isip ng kabataan\n"
+        "Kami ay dumating nang salat sa yaman\n"
+        "Hanap na dunong ay iyong alay\n"
+        "Ang layunin mong makatao\n"
+        "Dinarangal ang Pilipino\n"
+        "Ang iyong aral, diwa, adhikang taglay\n"
+        "PUP, aming gabay\n"
+        "Paaralang dakila\n"
+        "PUP, pinagpala\n"
+        "Gagamitin ang karunungan\n"
+        "Mula sa iyo, para sa bayan\n"
+        "Ang iyong aral, diwa, adhikang taglay\n"
+        "PUP, aming gabay\n"
+        "Paaralang dakila\n"
+        "PUP, pinagpala\n"
+    )
+    lines = hymn.split('\n')
+    max_len = max(len(line) for line in lines)
+    hymn = "\n".join(line.center(max_len) for line in lines)
 
-    for line in hymn:
-        print(line)
+    print(hymn)
 
 if __name__ == "__main__":
-    print_tree_hymn()
+    print_hymn()
+
+
+
+
