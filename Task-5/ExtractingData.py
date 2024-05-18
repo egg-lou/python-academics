86cf6c164114c4cd7e6e5dc1dab7aaa68e63a488
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-18 18:47:13 +0800

    done extracting data

---
+++ Task-5/ExtractingData.py
@@ -0,0 +1,10 @@
+students = [["BSIT", ["David", "Alenere"]], ["BSCS", ["Jaymar", "Emman", "Patrick"]]]
+
+
+def display_students(course_students):
+    for course in course_students:
+        print(course[0])
+        for student in course[1]:
+            print(f"   - {student}")
+
+display_students(students)
